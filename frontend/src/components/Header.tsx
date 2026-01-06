af84c834158b026d2128de432ae0fcca92d83de5
D0ugins <yusufishabazz@gmail.com>
2026-01-06 13:47:51 -0500

    make header smaller

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from '@tanstack/react-router'
 export default function Header() {
   return (
     <>
-      <header className="p-4 flex items-center bg-gray-800 text-white shadow-lg">
+      <header className="p-2 flex items-center bg-gray-800 text-white shadow-lg">
         <h1 className="ml-4 text-xl">
           <Link to="/">
             Home
