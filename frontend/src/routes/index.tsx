dacbf6795dba8c68510186f9c32327ed3c4dc80d
D0ugins <yusufishabazz@gmail.com>
2026-01-10 15:39:42 -0500

    add info page

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -6,8 +6,10 @@ export const Route = createFileRoute('/')({
 
 function App() {
   return (
-    <div className="text-center">
-      Hi :)
+    <div className="m-8 text-lg">
+      Click Rolls in the header to get started <br />
+      <a className='text-blue-600 hover:underline'
+        href="https://docs.google.com/document/d/1dIadUSLrxL2oTqxEv8UGYbaSsMx7pDwcmHm6EAhIqW0/edit?tab=t.0">Explanation Doc</a>
     </div>
   )
 }
