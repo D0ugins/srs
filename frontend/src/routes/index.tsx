595547654608ac517d291014d574feb1cefde5b1
yusuf <yusufishsbazz@gmail.com>
2026-06-16 18:10:38 -0400

    update explanation doc link

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="m-8 text-lg">
       Click Rolls in the header to get started <br />
       <a className='text-blue-600 hover:underline'
-        href="https://docs.google.com/document/d/1dIadUSLrxL2oTqxEv8UGYbaSsMx7pDwcmHm6EAhIqW0/edit?tab=t.0">Explanation Doc</a>
+        href="https://docs.google.com/document/d/1IEYC55YR-bWA3fONGtN9pSZBQ_bkSnVQFTVOl_ltdnI/edit">Explanation Doc</a>
     </div>
   )
 }
